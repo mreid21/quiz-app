000191463fbd2db2750ed752630ca38529b6b02c
= <n01437978@unf.edu>
2022-03-17 21:53:57 -0400

    added questions

---
+++ src/static/questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+        id: 1,
+        question: 'Which HTML attribute points to images, audio files, and video clips?',
+        answers: [
+            {letter: 'A', content: 'id'},
+            {letter: 'B', content: 'href'},
+            {letter: 'C', content: 'src'},
+            {letter: 'D', content: 'class'}
+        ],
+        correctAnswer: 'C'
+    },
+    {
+        id: 2,
+        question: 'Java can run on any computer platform that has a ...?',
+        answers: [
+            {letter: 'A', content: 'Java Compiler'},
+            {letter: 'B', content: 'Linux OS'},
+            {letter: 'C', content: 'Windows 10 OS'},
+            {letter: 'D', content: 'Java Virtual Machine'}
+        ],
+        correctAnswer: 'D'
+    },
+    {
+        id: 3,
+        question: 'Which of these is true about a set?',
+        answers: [
+            {letter: 'A', content: 'The elements are kept in order'},
+            {letter: 'B', content: 'There are no duplicates'},
+            {letter: 'C', content: 'They can only hold numbers'},
+            {letter: 'D', content: 'All of the above'}
+        ],
+        correctAnswer: 'B'
+    },
+    {
+        id: 4,
+        question: 'Which SQL aggregate function will get the number of rows returned from a query?',
+        answers: [
+            {letter: 'A', content: 'COUNT'},
+            {letter: 'B', content: 'SUM'},
+            {letter: 'C', content: 'MIN'},
+            {letter: 'D', content: 'MAX'}
+        ],
+        correctAnswer: 'A'
+    },
+    {
+        id: 5,
+        question: 'Which CSS "position" property will position an element relative to its nearest "positioned" ancestor?',
+        answers: [
+            {letter: 'A', content: 'absolute'},
+            {letter: 'B', content: 'fixed'},
+            {letter: 'C', content: 'relative'},
+            {letter: 'D', content: 'static'}
+        ],
+        correctAnswer: 'C'
+    },
+]
