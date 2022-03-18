725f07406b3fabf4e5c7d5a40244796627e4177b
= <n01437978@unf.edu>
2022-03-17 22:01:06 -0400

    changed question structure

--- src/static/questions.js
+++ src/static/questions.js
@@ -3,55 +3,50 @@ const questions = [
         id: 1,
         question: 'Which HTML attribute points to images, audio files, and video clips?',
         answers: [
-            {letter: 'A', content: 'id'},
-            {letter: 'B', content: 'href'},
-            {letter: 'C', content: 'src'},
-            {letter: 'D', content: 'class'}
-        ],
-        correctAnswer: 'C'
+            {letter: 'A', content: 'id', isCorrect: false},
+            {letter: 'B', content: 'href', isCorrect: false},
+            {letter: 'C', content: 'src', isCorrect: true},
+            {letter: 'D', content: 'class', isCorrect: false}
+        ]
     },
     {
         id: 2,
         question: 'Java can run on any computer platform that has a ...?',
         answers: [
-            {letter: 'A', content: 'Java Compiler'},
-            {letter: 'B', content: 'Linux OS'},
-            {letter: 'C', content: 'Windows 10 OS'},
-            {letter: 'D', content: 'Java Virtual Machine'}
-        ],
-        correctAnswer: 'D'
+            {letter: 'A', content: 'Java Compiler', isCorrect: false},
+            {letter: 'B', content: 'Linux OS', isCorrect: false},
+            {letter: 'C', content: 'Windows 10 OS', isCorrect: false},
+            {letter: 'D', content: 'Java Virtual Machine', isCorrect: true}
+        ]
     },
     {
         id: 3,
         question: 'Which of these is true about a set?',
         answers: [
-            {letter: 'A', content: 'The elements are kept in order'},
-            {letter: 'B', content: 'There are no duplicates'},
-            {letter: 'C', content: 'They can only hold numbers'},
-            {letter: 'D', content: 'All of the above'}
+            {letter: 'A', content: 'The elements are kept in order', isCorrect: false},
+            {letter: 'B', content: 'There are no duplicates', isCorrect: true},
+            {letter: 'C', content: 'They can only hold numbers', isCorrect: false},
+            {letter: 'D', content: 'All of the above', isCorrect: false}
         ],
-        correctAnswer: 'B'
     },
     {
         id: 4,
         question: 'Which SQL aggregate function will get the number of rows returned from a query?',
         answers: [
-            {letter: 'A', content: 'COUNT'},
-            {letter: 'B', content: 'SUM'},
-            {letter: 'C', content: 'MIN'},
-            {letter: 'D', content: 'MAX'}
-        ],
-        correctAnswer: 'A'
+            {letter: 'A', content: 'COUNT', isCorrect: true},
+            {letter: 'B', content: 'SUM', isCorrect: false},
+            {letter: 'C', content: 'MIN', isCorrect: false},
+            {letter: 'D', content: 'MAX', isCorrect: false}
+        ]
     },
     {
         id: 5,
         question: 'Which CSS "position" property will position an element relative to its nearest "positioned" ancestor?',
         answers: [
-            {letter: 'A', content: 'absolute'},
-            {letter: 'B', content: 'fixed'},
-            {letter: 'C', content: 'relative'},
-            {letter: 'D', content: 'static'}
-        ],
-        correctAnswer: 'C'
+            {letter: 'A', content: 'absolute', isCorrect: true},
+            {letter: 'B', content: 'fixed', isCorrect: false},
+            {letter: 'C', content: 'relative', isCorrect: false},
+            {letter: 'D', content: 'static', isCorrect: false}
+        ]
     },
 ]
