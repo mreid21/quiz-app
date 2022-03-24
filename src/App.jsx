de4af31b71e72cec8b43b79a6446127849133039
= <n01437978@unf.edu>
2022-03-23 21:48:58 -0400

    cleaned up css

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,38 @@ import "./App.css";
 function App() {
   return (
     <div className="app-container">
-      <div className="quiz-box rounded-corners">
+      <div className="quiz-box">
         <p className="quiz-box__question">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
           velit qui numquam delectus atque culpa eaque accusamus, rem quisquam
           beatae!
         </p>
-        <div className="quiz-box__answer">
-          <div className="answer__content rounded-corners transition t-ease t-duration-short">
-            <div className="quiz-box__letter">
+        <section id="answers">
+          <div className="quiz-box__answer rounded-corners transition t-ease t-duration-short">
+            <div className="quiz-box__answer__letter">
               <span>A</span>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-        </div>
-        <div className="quiz-box__answer">
-          <div className="answer__content rounded-corners transition t-ease t-duration-short">
-            <div className="quiz-box__letter">
+          <div className="quiz-box__answer rounded-corners transition t-ease t-duration-short">
+            <div className="quiz-box__answer__letter">
               <span>B</span>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-        </div>
-        <div className="quiz-box__answer">
-          <div className="answer__content rounded-corners transition t-ease t-duration-short">
-            <div className="quiz-box__letter">
+          <div className="quiz-box__answer rounded-corners transition t-ease t-duration-short">
+            <div className="quiz-box__answer__letter">
               <span>C</span>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-        </div>
-        <div className="quiz-box__answer">
-          <div className="answer__content rounded-corners transition t-ease t-duration-short">
-            <div className="quiz-box__letter">
+          <div className="quiz-box__answer rounded-corners transition t-ease t-duration-short">
+            <div className="quiz-box__answer__letter">
               <span>D</span>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   );
